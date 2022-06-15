11d3489b61ca960d69eec4272d108c0b09a4fa1a
Chloe Chen <cc126@rice.edu>
2022-06-14 22:37:49 -0500

    finished entire project

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profile_pic from "./assets/froggy_cup.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        {/* splits the page in half */}
+        {/* left side */}
+        <div>
+          <div className='split left'>
+            <div className='centered'>
+              <img className='profile_image' src={profile_pic} alt='Profile Pic'></img>
+            </div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div>
+          <div className='split right'>
+            <div className='centered'>
+              <div className='name_title'>Chloe Chen</div>
+              <div className='brief_description'>
+                {/* brief_description */}
+                Hi! I am a junior CS major at Rice University.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
